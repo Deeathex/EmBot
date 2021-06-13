74e1ccb6bb38f094fcee9978b3f77a14fb1fc3d9
Andreea Ciforac <andreea.ciforac@tora.com>
2021-06-13 20:29:17 +0300

    Refactor recommendation based on mental state

--- chatbot_apis/mental_state/mental_status_prediction.py
+++ chatbot_apis/mental_state/mental_status_prediction.py
@@ -45,15 +45,3 @@ class MentalStatusPredictor:
         mental_status_predictor = MentalStatusPredictor(EmotionClassifier(PATH_TO_MODEL))
         tokenized_text = sent_tokenize(text)
         return mental_status_predictor.get_mental_state(tokenized_text)
-
-
-mental_status_predictor = MentalStatusPredictor(EmotionClassifier(PATH_TO_MODEL))
-# conversation1 = ['Today, honey, is a very beautiful day!', 'I wish to die soon..',
-#                  "I want to die, I can't cope anymore!", 'I am so scared right now', 'All I want is some peace']
-# print(mental_status_predictor.run(conversation1))
-# conversation2 = ['Today, everything was awful!', 'I wish to die soon..',
-#                  "I want to die, I can't cope anymore!", 'I am so scared right now', 'All I want is some peace']
-# print(mental_status_predictor.run(conversation2))
-
-conversation3 = "I am not scared. I am getting better... Even if I am not, I try this. I thought life would be easier but it isn't"
-print(mental_status_predictor.run(conversation3))
--- chatbot_apis/mental_state/recommendation_based_on_mental_state.py
+++ chatbot_apis/mental_state/recommendation_based_on_mental_state.py
@@ -1,46 +1,41 @@
-from chatbot_apis.mental_state.mental_status_prediction import MentalStatusPredictor
+import json
 
 from random import randrange
+
+from chatbot_apis.mental_state.mental_status_prediction import MentalStatusPredictor
+
+RECOMMENDATIONS_BY_MENTAL_STATUS = 'recommendations_by_mental_status'
 
 RECOMMENDATION = 'recommendation'
 MENTAL_STATE = 'mental_state'
 
+PATH_JSON = 'D:\\Tot\\UBB\\SDI\\Semestrul_4\\Disertatie\\Parte practica\\Embot_nlp_backend\\chatbot_apis\\mental_state\\recommendations_by_mental_state.json'
+
 
 class RecommendationBasedOnMentalState:
     def __init__(self):
-        self.recommendations_by_mental_status = {
-            'positive': ["I am glad you're feeling great!"],
-            'normal': ["I am glad you're feeling great!"],
-            'slightly stressed': [
-                'You should think of some activities or hobbies you enjoy and practice them on a regular basis.',
-                'You should meet up with your friends sometime and enjoy some mutual activities.',
-                'I would suggest looking for a hobby group in your area.',
-            ],
-            'highly stressed': [
-                'You should identify the stress sources from your life and reduce them.',
-                'Try to think about what causes you the stress and try to avoid it.',
-                'Try to discuss the stressful events in your life with someone close to you.',
-            ],
-            'slightly depressed': [
-                'Try to relax from time, to time. Try to meditate and free your mind from the negative thoughts.',
-                'You should try meditation sometime, it might help clear your mind.',
-                'Everyone has bad days once in a while, but it is important not to generalize the negative thoughts however.',
-            ],
-            'highly depressed': [
-                'When we are very sad for a longer period of time, we tend to alter the reality. Try to think about your emotions and feelings and try to analyse them. They are not good for you and your health.',
-                'I know that everything seems to be falling apart, but it will get better if you focus on your positive thoughts, rather than the negative ones.',
-                'There are moments when we think that our future is not in our hands, but those moments and the way we are coping with them matter. When in need, try to think about a pleasant memory that gives you the strength you need to overcome the challenges you are facing.',
-            ],
-        }
+        self.__json_file = PATH_JSON
+        self.__recommendations_by_mental_status = self.__load_practices()
 
     def get_recommendation(self, text):
         mental_state = MentalStatusPredictor.run(text)
+        recommendations = []
+        for pair in self.__recommendations_by_mental_status[RECOMMENDATIONS_BY_MENTAL_STATUS]:
+            if pair[MENTAL_STATE] == mental_state:
+                recommendations = pair['recommendation']
         data = {
             MENTAL_STATE: mental_state,
-            RECOMMENDATION: self.__get_random_answer(self.recommendations_by_mental_status[mental_state])
+            RECOMMENDATION: self.__get_random_answer(recommendations)
         }
         return data
 
-    def __get_random_answer(self, list):
+    def __load_practices(self):
+        f = open(self.__json_file, encoding='utf-8')
+        data = json.load(f)
+        f.close()
+        return data
+
+    @staticmethod
+    def __get_random_answer(list):
         position = randrange(len(list))
         return list[position]
